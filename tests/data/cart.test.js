c6797199663242cfc33c1ecf679be581b17a8020
scartisp <simion.cartis1@gmail.com>
2026-01-17 00:53:06 -0500

    strengthened test cases for cart

--- tests/data/cart.test.js
+++ tests/data/cart.test.js
@@ -9,17 +9,26 @@ beforeEach(() => {
 describe('testing addToCart', () => {
   test('adds a new product to the cart', () => {
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 1);
-    
+
     expect(cart.length).toBe(1);
     expect(cart[0].productId).toBe('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toBe(1);
   });
+
+  test('adds two instance of a new product to the cart', () => {
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 2);
+
+    expect(cart.length).toBe(1);
+    expect(cart[0].productId).toBe('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toBe(2);
+  });
 });
 
-describe ('testing addQuantity', () => {
+describe('testing addQuantity', () => {
   test('adds an existing product to the cart', () => {
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 1);
     addQuantity(0, 1);
+    
     expect(cart.length).toBe(1);
     expect(cart[0].productId).toBe('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toBe(2);
