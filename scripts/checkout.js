9543fa6cc4d7059c53e2fd058fa3d9571e7816b6
scartisp <simion.cartis1@gmail.com>
2026-03-09 22:00:54 -0400

    Revert "Merge branch 'backend' into testing"
    This reverts commit cc8dd727a1cc70838657c8c77150e5451eedc98f, reversing
    changes made to 3e2c047b0529a5d5a4e49ff3f7c85cb9fb601f90.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,27 +1,10 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { loadProducts, products } from "../data/products.js";
+import { loadProducts } from "../data/products.js";
 
-
-loadProducts().then(() => { // after products are loaded, render the page.
+loadProducts(() => {
   renderOrderSummary();
   renderPaymentSummary();
   renderCheckoutHeader();
-})
-
-
-
-/*you can run multiple promises simultaneously with promise.all() 
-ex:
-Promise.all([
-  new Promise((resolve) => {
-    resolve('something');
-  }),
-  new Promise((resolve) => {
-    resolve('something else');
-  })
-]).then((values)=> { //values here is an array of what was passed into the resolves
-    something to do after
-})
-*/
+});
