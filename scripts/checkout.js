e7ce18ac07283ef5f463c6d558a8427541d7e08b
scartisp <simion.cartis1@gmail.com>
2026-03-09 21:36:22 -0400

    used fetch to load the products

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,20 +1,14 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, products } from "../data/products.js";
 
 
-new Promise((resolve) => {// promises are better than callbacks because multiple callbacks create a lot of nesting
-  console.log('start promise');
-  loadProducts(() => {
-    resolve(); //reslove finishes the function Promise takes and then runs 'then'
-  });// resolve can take an argument. The argument will be passed as the first paramter for 'then'
-
-}).then(() => { // after products are loaded, render the page.
+loadProducts().then(() => { // after products are loaded, render the page.
   renderOrderSummary();
   renderPaymentSummary();
   renderCheckoutHeader();
-});// 'thens' can have Promises in them as well
+})
 
 
 
